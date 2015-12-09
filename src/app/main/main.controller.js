f1da0508c699775c6eda03e04d139895c82e3333
James King <j@lostwith.me>
2015-12-09 12:27:21 +0000

    get film list and compare against hidden list

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,10 +1,36 @@
 export class MainController {
-  constructor ($scope, $log, api) {
+  constructor ($scope, $log, Api, $localStorage, $filter) {
     'ngInject';
 
-    api.get('cinemas').then(function (response) {
-      $scope.result = response;
+    $scope.clean = true;
+
+    $scope.cinema = $localStorage.cinema || {};
+    $scope.hidden = $localStorage.hidden || {};
+
+    Api.get('cinemas').then(function (response) {
+      $scope.cinemas = response;
     });
+
+    Api.get('films', {cinema: $scope.cinema, date: 20151210, full: true}).then(function (films) {
+
+      for (let edi in $scope.hidden) {
+        // $log.debug(edi);
+        let film = $filter('filter')(films, {edi: parseInt(edi)}, true)[0];
+        // $log.debug(film);
+        if (film) {
+          film.hidden = $scope.hidden[edi];
+        } else {
+          $log.debug('removed old film from hidden list');
+          delete $scope.hidden[edi];
+        }
+      }
+
+      $scope.films = films;
+    });
+
+    $scope.saveCinema = function () {
+      $localStorage.cinema = $scope.cinema;
+    };
 
   }
 
