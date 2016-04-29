376ab81b33d6d40b155562cda88cf25526b4b686
James King <james@wearecrystal.uk>
2016-04-29 17:40:13 +0100

    remove default date from api

--- app/components/api.service.js
+++ app/components/api.service.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('gulp-angular')
-  .factory('Api', function ($resource, $localStorage, moment) {
+  .factory('Api', function ($resource, $localStorage) {
     let apiKey = 'TNk2:R3P';
     return $resource('http://www.cineworld.com/api/quickbook/:route',
-      {key: apiKey, cinema: $localStorage.cinema, callback: 'JSON_CALLBACK', date: moment().format('YYYYMMDD')}, {
+      {key: apiKey, cinema: $localStorage.options.cinema, callback: 'JSON_CALLBACK'}, {
         cinemas: {method: 'JSONP', params: {route: 'cinemas', cinema: null}},
-        films: {method: 'JSONP', params: {full: true}},
+        films: {method: 'JSONP', params: {route: 'films', full: true}},
         shows: {method: 'JSONP'}
       }
     );
