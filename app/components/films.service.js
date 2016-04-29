cb43710b2392e8a9fb24dfe953486378f05933ab
James King <james@wearecrystal.uk>
2016-04-29 17:38:25 +0100

    separate out film service and build film sorting and combining system

---
+++ app/components/films.service.js
@@ -0,0 +1,89 @@
+'use strict';
+
+angular.module('gulp-angular')
+  .factory('Films', function ($q, $window, $log, $http, $localStorage, Api) {
+    return {
+
+      get: function () {
+        let self = this;
+
+        return $q(function (resolve, reject) {
+
+          if ($localStorage.films) {
+            $log.debug('local films');
+            resolve($localStorage.films);
+          } else {
+            $log.debug('remote films');
+
+            Api.films(function (films) {
+
+              let converted = films.films.map(self.convert);
+
+              angular.forEach(converted, function (film, i) {
+                if (film) {
+                  for (var i2 = i + 1; i2 < converted.length; i2++) {
+                    if (converted[i2].title === film.title) {
+                      angular.extend(film.types, converted[i2].types);
+                      converted[i2] = '';
+                    }
+                  }
+                }
+              });
+
+              films = converted.filter(film => film);
+
+              $localStorage.films = films;
+              resolve(films);
+
+            }, function (error) {
+              reject(error);
+            });
+
+          }
+
+        });
+
+      },
+
+      convert: function (inFilm) {
+        let type;
+
+        let reg = {
+          three: /^\(3[dD]\) /,
+          imax: /^\(IMAX\) /,
+          two: /^\(2[dD]\) /,
+          i3d: /^\(IMAX 3-?[dD]\) /
+        };
+
+        let outFilm = {
+          title: inFilm.title,
+          url: inFilm.inFilm_url,
+          classification: inFilm.classification,
+          advisory: inFilm.advisory,
+          poster: inFilm.poster_url,
+          still: inFilm.still_url,
+          types: {}
+        };
+
+        if (reg.three.test(inFilm.title)) {
+          type = 'three';
+        } else if (reg.imax.test(inFilm.title)) {
+          type = 'imax';
+        } else if (reg.i3d.test(inFilm.title)) {
+          type = 'i3d';
+        } else {
+          type = 'two';
+        }
+
+        outFilm.title = inFilm.title.replace(reg[type], '');
+        outFilm.types[type] = {
+          edi: inFilm.edi,
+          poster: inFilm.poster_url
+        };
+
+        return outFilm;
+      }
+
+    };
+  })
+;
