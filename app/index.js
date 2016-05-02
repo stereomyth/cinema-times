8789ae876b90b61c7a8dcb12845093cdb9e043c9
James King <j@lostwith.me>
2016-05-02 13:44:25 +0100

    tab type none, and set as default

--- app/index.js
+++ app/index.js
@@ -21,7 +21,7 @@
     .config(function ($logProvider) {
       $logProvider.debugEnabled(true);
       if (!localStorage['ngStorage-options']) {
-        localStorage['ngStorage-options'] = '{}';
+        localStorage['ngStorage-options'] = '{"typeTabs": "none"}';
       }
     })
 
