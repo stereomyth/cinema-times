041cf454a8a3b29c33e8ebb9e2a3223ca10a32c9
James King <james@wearecrystal.uk>
2016-05-25 17:36:57 +0100

    kill dead code, tidy etc

--- app/views/main/main.controller.js
+++ app/views/main/main.controller.js
@@ -8,18 +8,12 @@
 
       // $scope.clean = true;
 
-      $scope.all = true;
-
       $scope.options = $localStorage.options;
-      // $localStorage.general = $localStorage.general || {};
 
-      // Res.get('events').then(events => { $scope.events = events; });
-      Res.get('cinemas').then(cinemas => { $scope.cinemas = cinemas; });
-      Res.get('today').then(today => { $scope.today = today; });
+      Res.get('cinemas', {cinema: null}).then(cinemas => { $scope.cinemas = cinemas; });
 
       let getFilms = () => {
-        Films.get({cinema: $scope.options.cinema})
-          .then(films => { $scope.films = films; });
+        Films.get().then(films => { $scope.films = films; });
       };
 
       if ($scope.options.cinema) {
@@ -27,24 +21,6 @@
       } else {
         $scope.showOptions = true;
       }
-
-      // let getFilms = () => {
-      //   Api.get('films').then(films => {
-
-      //     for (let edi in $scope.hidden) {
-      //       let film = $filter('filter')(films, {edi: parseInt(edi)}, true)[0];
-
-      //       if (film) {
-      //         film.hidden = $scope.hidden[edi];
-      //       } else {
-      //         $log.debug('removed old film from hidden list');
-      //         delete $scope.hidden[edi];
-      //       }
-      //     }
-
-      //     $scope.films = films;
-      //   });
-      // };
 
       $scope.selectCinema = () => {
         delete $localStorage.films;
