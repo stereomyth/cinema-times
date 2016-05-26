65398491a11996f7f67467d78ab6922152faf81e
James King <james@wearecrystal.uk>
2016-05-26 16:52:30 +0100

    fix indentation

--- app/views/main/main.controller.js
+++ app/views/main/main.controller.js
@@ -1,42 +1,39 @@
+'use strict';
 
-  'use strict';
+angular.module('cineworld')
+  .controller('MainController', function($scope, $log, $localStorage, Res, Films) {
 
-  angular.module('cineworld')
-    .controller('MainController', function($scope, $log, $localStorage, Res, Films) {
+    $scope.options = $localStorage.options;
 
-      $scope.options = $localStorage.options;
+    Res.get('cinemas', {cinema: null}).then(cinemas => { $scope.cinemas = cinemas; });
 
-      Res.get('cinemas', {cinema: null}).then(cinemas => { $scope.cinemas = cinemas; });
+    let getFilms = () => {
+      Films.get().then(films => { $scope.films = films; });
+    };
 
-      let getFilms = () => {
-        Films.get().then(films => { $scope.films = films; });
-      };
-
-      if ($scope.options.cinema) {
-        getFilms();
-      } else {
-        $scope.showOptions = true;
-      }
-
-      $scope.selectCinema = () => {
-        delete $localStorage.films;
-        getFilms();
-      };
-
-      $scope.clearAll = () => {
-        $localStorage.$reset();
-      };
-
-      $scope.clearApi = () => {
-        delete $localStorage.films;
-        delete $localStorage.cinemas;
-      };
-
-      $scope.clearHidden = () => {
-        $localStorage.options.hidden = [];
-      };
-
+    if ($scope.options.cinema) {
+      getFilms();
+    } else {
+      $scope.showOptions = true;
     }
-  );
 
+    $scope.selectCinema = () => {
+      delete $localStorage.films;
+      getFilms();
+    };
 
+    $scope.clearAll = () => {
+      $localStorage.$reset();
+    };
+
+    $scope.clearApi = () => {
+      delete $localStorage.films;
+      delete $localStorage.cinemas;
+    };
+
+    $scope.clearHidden = () => {
+      $localStorage.options.hidden = [];
+    };
+
+  })
+;
