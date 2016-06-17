08093a9815fe53611d69d79b6925ea610fa0e2a1
James King <james@wearecrystal.uk>
2016-06-17 14:55:11 +0100

    disable film directive

--- app/components/film/film.directive.js
+++ app/components/film/film.directive.js
@@ -1,36 +1,27 @@
 'use strict';
 
 angular.module('cineworld')
-  .directive('film', function ($localStorage) {
+  .directive('film', function () {
     return {
 
       restrict: 'A',
-      templateUrl: 'components/film/film.html',
-      scope: {
-        film: '='
-      },
+      templateUrl: 'components/film/film.html'
+      // scope: {
+      //   film: '='
+      // },
 
-      link: function (scope) {
-        let hidden = $localStorage.options.hidden;
+      // link: function (scope) {
+      //   let hidden = $localStorage.options.hidden;
 
-        scope.toggleHidden = film => {
-          if (film.hidden) {
-            for (let type in film.types) {
-              hidden.splice(hidden.findIndex(element => element === film.types[type].edi), 1);
-            }
-          } else {
-            for (let type in film.types) {
-              hidden.push(film.types[type].edi);
-            }
-          }
-          film.hidden = !film.hidden;
-        };
 
-        // scope.v = {
-        //   shows: true
-        // };
 
-      }
+      //   // scope.v = {
+      //   //   shows: true
+      //   // };
+
+      //   scope.objl = obj => Object.keys(obj).length;
+
+      // }
 
     };
   })
