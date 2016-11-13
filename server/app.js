872b7dc3ffa1de50a19cf7eb4fb557b6b09a70de
James King <j@lostwith.me>
2016-11-13 09:44:38 +0000

    move api to /api

--- server/app.js
+++ server/app.js
@@ -24,8 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/films', films);
-app.use('/cinemas', cinemas);
+app.use('/api/films', films);
+app.use('/api/cinemas', cinemas);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
