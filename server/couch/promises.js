7e2a33556103b2cc5b387f31c61b0b28fc35816c
James King <james@wearecrystal.uk>
2016-11-17 18:00:02 +0000

    Build promise wrappers for nano functions

---
+++ server/couch/promises.js
@@ -0,0 +1,55 @@
+let nano = require('nano')('http://localhost:5984');
+
+let db = nano.db.use('cineworld-one');
+
+// ========
+// Just some promise wrappers around the basic Nano functions
+// ========
+
+let tiny = {
+
+  get: (doc) => {
+    return new Promise ((resolve, reject) => {
+
+      db.get(doc, (err, body) => {
+        if (!err) {
+          resolve(body);
+        } else {
+          reject(err);
+        }
+      });
+
+    });
+  },
+
+  insert: (doc, name) => {
+    return new Promise ((resolve, reject) => {
+
+      db.insert(doc, name, (err, body) => {
+        if (!err) {
+          resolve(body);
+        } else {
+          reject(err);
+        }
+      });
+    
+    });
+  },
+
+  view: (doc, view, params = {}) => {
+    return new Promise ((resolve, reject) => {
+
+      db.view(doc, view, params, function (err, body, headers) {
+        if (!err) {
+          resolve(body);
+        } else {
+          reject(err);
+        }
+      });
+
+    });
+  }
+
+};
+
+module.exports = tiny;
