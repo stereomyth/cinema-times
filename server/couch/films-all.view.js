4fbd323834d04331282d034e44ec18d5c6e32fe1
James King <james@wearecrystal.uk>
2016-11-17 12:16:13 +0000

    View for displaying films ready to flatten into final json

---
+++ server/couch/films-all.view.js
@@ -0,0 +1,18 @@
+function (doc, thing) {
+  if (doc.type === 'film') {
+    for (cinema in doc.screenings) {
+      var film = {
+        title: doc.title,
+        poster: doc.poster,
+        variants: [{
+          type: doc.variant,
+          edi: doc._id,
+          oldTitle: doc.oldTitle,
+          screenings: doc.screenings
+        }]
+      }
+
+      emit([cinema, doc.title], film);  
+    }
+  }
+}
