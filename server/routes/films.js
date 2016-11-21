62ed6e2a21ce4c538d19d16462714197902a495a
James King <james@wearecrystal.uk>
2016-11-21 17:22:33 +0000

    Big rebuild of stamp checking, only get films not on same day

--- server/routes/films.js
+++ server/routes/films.js
@@ -9,40 +9,6 @@ var getFilms = require('../api/getFilms.js');
 let films = [];
 let events;
 
-let stamps = (name, time) => {
-  return new Promise((resolve, reject) => {
-
-    db.get('stamps', (err, body) => {
-      if (!err) {
-
-        if (time) {
-          body[name] = time;
-
-          db.insert(body, err => {
-            if (!err) {
-              resolve(true);
-            } else {
-              reject(err);
-            }
-          });
-
-        } else {
-          if (body[name]) {
-            resolve(body[name]);
-          } else {
-            resolve(moment('1995-12-25'));
-          }
-        }
-
-      } else {
-        reject(err);
-      }
-
-    });
-
-  });
-};
-
 router.get('/', function(req, res, next) {
   res.send({
     error: 'cinema id is required: api/films/[cinemaId]'
@@ -50,8 +16,6 @@ router.get('/', function(req, res, next) {
 });
 
 let flatten = (film, cinema) => {
-  film.variants[0].screenings = film.variants[0].screenings[cinema];
-
   if (films.length && film.title === films[films.length -1].title) {
     films[films.length -1].variants.push(film.variants[0]);
   } else {
@@ -62,41 +26,24 @@ let flatten = (film, cinema) => {
 
 router.get('/:cinemaId', function(req, res, next) {
 
-  // stamps('films' + req.params.cinema).then(function (response) {
-  //   if (moment(response).isAfter(moment().subtract(12, 'hours'))) {
-  //     console.log('get local films  ------------------');
+  tiny.get('stamps').then(stamps => {
+    let stamp = stamps['films' + req.params.cinemaId];
 
-      films = [];
-
-      // use local films
-      let params = {
-        startkey: [req.params.cinemaId, 0], 
-        endkey: [req.params.cinemaId + 1, 0]
-      };
-
-      tiny.view('films', 'all', params).then(body => {
-        body.rows.forEach( row => {
-          flatten(row.value, req.params.cinemaId);
-        });
-
-        res.send(films);
-      }).catch(error => {
-        res.send(error);
+    if (!stamp || !moment(stamp).subtract(1, 'hour').isSame(moment(), 'day')) {
+      return getFilms(req.params.cinemaId).then(films => {
+        stamps['films' + req.params.cinemaId] = moment();
+        return tiny.insert(stamps);
       });
+    }
+  })
+  .then(() => tiny.view('films', 'all', { startkey: [req.params.cinemaId, 0], endkey: [req.params.cinemaId + 1, 0] }))
+  .then(view => {
+    view.rows.forEach(row => {
+      flatten(row.value, req.params.cinemaId);
+    });
 
-    // } else {
-
-      // get remote films
-      // getFilms(req.params.cinemaId).then(
-      //   response => {
-      //     res.send(response);
-      //   }
-      // ).catch(error => res.send(error));
-
-    // }
-  // }).catch(error => {
-  //   res.send(error);
-  // });
+    res.send(films);
+  }).catch(error => res.send(error));
 
 });
 
