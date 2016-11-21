239436b13b77154d773a53fb112d427b57835940
James King <james@wearecrystal.uk>
2016-11-21 15:21:22 +0000

    Rebuild cinemas route with new promises

--- server/routes/cinemas.js
+++ server/routes/cinemas.js
@@ -1,63 +1,31 @@
-var express = require('express');
-var router = express.Router();
-var moment = require('moment');
+let express = require('express');
+let router = express.Router();
+let moment = require('moment');
 
-var nano = require('nano')('http://localhost:5984');
-var db = nano.db.use('cineworld-one');
+let api = require('../api/api.js');
+let tiny = require('../couch/promises.js');
 
-var api = require('../api/api.js');
-
-
-var getCinemas = function (res, rev) {
-  console.log('remote - cinemas');
-
-  api('cinemas', function (error, response, body) {
-    var cinemas = {
-      _id: 'cinemas',
-      updated: new Date(),
-      cinemas: JSON.parse(body).cinemas, 
-    }
-
-    if (rev) {
-      cinemas._rev = rev;
-    }
-
-    db.insert(cinemas, function (err) {
-      if (err) 
-        console.log(err);
-    });
-
-    res.send(body);
-  });
-
+let getCinemas = () => {
+  return api('cinemas').then(cinemas => {
+    cinemas.updated = moment();
+    return tiny.insert(cinemas, 'cinemas');
+  })
 };
 
 router.get('/', function(req, res, next) {
 
-  // if no doc or out of date get cinemas otherwise send cinemas
-
-  db.get('cinemas', function(err, body) {
-    if (!err) {
-
-      if (body.updated && moment(body.updated).isAfter(moment().subtract(14, 'days'))) {
-        console.log('good date');
-        console.log('local - cinemas');
-
-        res.send(body.cinemas);
-      } else {
-        // if older than 14 days 
-        console.log('bad date');
-        getCinemas(res, body._rev);
-      }
-
+  tiny.get('cinemas').then(cinemas => {
+    if (moment(cinemas.updated).add(14, 'days').isBefore(moment())) {
+      return getCinemas();
     } else {
-      if (err.statusCode === 404) {
-        getCinemas(res);
-      } else {
-        res.send(err);
-      }
+      return cinemas;
     }
-  });
+  }, error => {
+    return getCinemas();
+  })
+  .then(cinemas => { res.send(cinemas.cinemas) })
+  .catch(error => { res.send(error) });
+
 });
 
 module.exports = router;
