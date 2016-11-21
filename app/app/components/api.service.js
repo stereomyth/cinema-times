e9b93e72b85822484ae0a115a5bef9553f0ac394
James King <james@wearecrystal.uk>
2016-11-21 18:23:00 +0000

    Switch app to local API

--- app/app/components/api.service.js
+++ app/app/components/api.service.js
@@ -2,9 +2,6 @@
 
 angular.module('cineworld')
   .factory('Api', function ($resource, $localStorage) {
-    let apiKey = 'TNk2:R3P';
-    return $resource('http://www.cineworld.com/api/quickbook/:route',
-      {key: apiKey, cinema: $localStorage.options.cinema, callback: 'JSON_CALLBACK'}, { get: {method: 'JSONP'} }
-    );
+    return $resource('http://localhost:3000/api/:route/:cinema', {cinema: $localStorage.options.cinema});
   })
 ;
